799c3760be157ca1c237c2da8d3e434a096db2c4
Collinsedumadze <egyiredumadzecollins@gmail.com>
2025-06-30 21:02:27 +0000

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<About />} />
         <Route path='/login' element={<Login />} />
       </Routes>
     </div>
